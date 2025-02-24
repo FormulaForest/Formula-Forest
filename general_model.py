5c688625cf6adea9d1e229da105463f3cd574546
Lev <lev_tartakovskyi@woodberry.org>
2025-02-24 13:19:06 -0500

    Created general_model file
    Co-authored-by: PennStClair <PennStClair@users.noreply.github.com>

---
+++ general_model.py
@@ -0,0 +1,17 @@
+import sympy as ap
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Car:
+
+    def __init__(self):
+        self.x = np.zeros(3)
+        self.v = np.zeros(3)
+        self.a = np.zeros(3)
+        self.t = 0
+        self.dt = 1e-4
+        self.m = 0.05
+
+    def 
+
+    def update(self):
